f48f552a01c0844d47b4ed9551728737bc6316e8
heatyux <421743974@qq.com>
2019-06-25 20:33:18 +0800

    第二个版本

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-$('.images > img:nth-child(1)').addClass('current');
-$('.images > img:nth-child(2)').addClass('enter');
-$('.images > img:nth-child(3)').addClass('enter');
-
 var n = 1
+initialize(getImage(n));
+
 setInterval(() => {
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e) => {
-      $(e.currentTarget).removeClass('leave').addClass('enter');
+      makeEnter($(e.currentTarget));
     })
-  $(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current');
+  makeCurrent(getImage(n + 1));
   n += 1;
 }, 3000);
 
+var images = $('.images > img').length; // 获取 'img' 数量
 function x(n) {
-  if(n > 3) {
-    n = n % 3
+  if(n > images) {
+    n = n % images
     if(n === 0) {
-      n = 3
+      n = images
     }
   }
   return n;
+}
+
+// 初始化添加状态机
+function initialize($node) {
+  $node.addClass('current')
+    .siblings().addClass('enter');
+}
+
+// 获得节点
+function getImage(n) {
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+// 添加 'current' 状态
+function makeCurrent($node) {
+  return $node.removeClass('enter leave').addClass('current');
+}
+
+// 添加 'leave' 状态
+function makeLeave($node) {
+  return $node.removeClass('current enter').addClass('leave')
+}
+
+// 添加 'enter' 状态
+function makeEnter($node) {
+  return $node.removeClass('leave current').addClass('enter')
 }
